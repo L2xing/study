3d10f7c8667195426ef301874baf0a1cdad8a392
liuxingxing.3011 <liuxingxing.3011@bytedance.com>
2025-01-02 18:40:54 +0800

    feat: del coordinator.go

--- 6_824/code/src/mr/coordinator.go
+++ 6_824/code/src/mr/coordinator.go
@@ -6,138 +6,28 @@ import (
 	"net"
 	"net/http"
 	"net/rpc"
-	// "os"
-	"strings"
-	"sync"
-	"time"
 )
 
 type Coordinator struct {
 	// Your definitions here.
+	files   []string
+	nReduce int
 
 	// mapper
-	mapperLock    sync.Mutex
-	mappers       map[string]bool
-	mapperShuffle map[string][]string
+	mappers map[string]bool
 
 	// ReducerKey
-	reducers map[string]bool
 
 	// all
-	mutexWork       sync.Mutex
-	idelWorks       []WorkerInfo
-	processingWorks []WorkerInfo
+
 }
 
 // Your code here -- RPC handlers for the worker to call.
 func (c *Coordinator) Register(args *RegisterArgs, reply *RegisterReply) error {
-	c.mutexWork.Lock()
-	defer c.mutexWork.Unlock()
-	workerAddr := args.Addr
-
-	hasWorker := false
-	for _, worker := range c.idelWorks {
-		compare := strings.Compare(workerAddr, worker.Addr)
-		if compare == 0 {
-			hasWorker = true
-			break
-		}
-	}
-	for _, worker := range c.processingWorks {
-		compare := strings.Compare(workerAddr, worker.Addr)
-		if compare == 0 {
-			hasWorker = true
-			break
-		}
-	}
-
-	if hasWorker {
-		reply.Success = true
-		return nil
-	}
-
-	c.idelWorks = append(c.idelWorks, WorkerInfo{Addr: args.Addr, State: 0})
-	reply.Success = true
 	return nil
 }
 
 func (c *Coordinator) MapDone(args *MapDoneArgs, reply *MapDoneReply) error {
-	c.mapperLock.Lock()
-	defer c.mapperLock.Unlock()
-	log.Printf("MapDone started, addr:%s, fileName:%s, shuffles:%v", args.Addr, args.FileName, len(args.Shuffles))
-	// 1. 如果 fileName已经处理过直接跳过
-	done, ok := c.mappers[args.FileName]
-	if ok && done {
-		log.Printf("MapDone done, addr:%s, fileName:%s \n", args.Addr, args.FileName)
-		reply.Success = true
-		c.releaseWorker(args.Addr)
-		return nil
-	}
-
-	// 2. 合并fileName的shuffle
-	c.mappers[args.FileName] = true
-	for k, v := range args.Shuffles {
-		shuffles, ok := c.mapperShuffle[k]
-		if !ok {
-			shuffles = make([]string, 1)
-			c.mapperShuffle[k] = shuffles
-		}
-		c.mapperShuffle[k] = append(shuffles, v...)
-	}
-
-	// 3. 释放一个worker
-	log.Printf("MapDone finished, addr:%s, fileName:%s \n", args.Addr, args.FileName)
-	reply.Success = true
-	c.releaseWorker(args.Addr)
-	return nil
-}
-
-func (c *Coordinator) releaseWorker(addr string) {
-	c.mutexWork.Lock()
-	defer c.mutexWork.Unlock()
-
-	// 1. 释放一个worker
-	delIdx := -1
-	var workerInfo WorkerInfo
-	for idx, worker := range c.processingWorks {
-		if strings.Compare(worker.Addr, addr) == 0 {
-			delIdx = idx
-			workerInfo = worker
-			break
-		}
-	}
-	if delIdx == -1 {
-		return
-	}
-
-	// 2. 恢复一个worker
-	c.processingWorks = append(c.processingWorks[:delIdx], c.processingWorks[delIdx+1:]...)
-	c.idelWorks = append(c.idelWorks, workerInfo)
-}
-
-func (c *Coordinator) applyWorker() string {
-	c.mutexWork.Lock()
-	defer c.mutexWork.Unlock()
-	fmt.Println("申请worker...")
-	if len(c.idelWorks) == 0 {
-		fmt.Println("当前没有空闲worker...")
-		return ""
-	}
-	idx := 0
-	workerInfo := c.idelWorks[idx]
-
-	c.processingWorks = append(c.processingWorks, workerInfo)
-	c.idelWorks = c.idelWorks[idx+1:]
-
-	fmt.Println("申请到worker... addr:", workerInfo.Addr)
-	return workerInfo.Addr
-}
-
-// an example RPC handler.
-
-// the RPC argument and reply types are defined in rpc.go.
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
 	return nil
 }
 
@@ -170,113 +60,16 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of ReducerKey tasks to use.
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	log.Printf("files:%v", files)
-	c := Coordinator{}
-	c.mappers = make(map[string]bool)
-	c.mapperShuffle = make(map[string][]string)
-
-	c.reducers = make(map[string]bool)
-	c.idelWorks = make([]WorkerInfo, 0)
-	c.processingWorks = make([]WorkerInfo, 0)
-
-	// Your code here.
-	// 1. 定期检测
-	go func() {
-		for {
-			idelCnt := 0
-			for _ = range c.idelWorks {
-				idelCnt++
-			}
-			processingCnt := 0
-			for _ = range c.processingWorks {
-				processingCnt++
-			}
-			totalCnt := idelCnt + processingCnt
-			log.Printf("worker count:%d, idelcnt:%d, processingCnt:%d\n", totalCnt, idelCnt, processingCnt)
-			time.Sleep(1 * time.Second)
-		}
-	}()
-
-	// 2. Task分配
-	for _, fileName := range files {
-		c.mappers[fileName] = false
-	}
-
-	go func() {
-		fmt.Println("task begin")
-
-		// 1. map分配
-		for {
-			boolAllDone := true
-			for fileName, done := range c.mappers {
-				if done {
-					continue
-				}
-				boolAllDone = false
-				// 1. 获取一个worker
-				workerAddr := c.applyWorker()
-				if strings.Compare(workerAddr, "") == 0 {
-					time.Sleep(1 * time.Second)
-					continue
-				}
-
-				// 2. 调用worker的MapReq
-				go func() {
-					success := CallMapReq(workerAddr, fileName)
-					if !success {
-						fmt.Println("call fail release addr:", workerAddr)
-						c.releaseWorker(workerAddr)
-					}
-				}()
-			}
-			if boolAllDone {
-				break
-			}
-			log.Println("in map")
-			time.Sleep(1 * time.Second)
-		}
-
-		// 2. reduce分配
-		for {
-			allDone := true
-			for k, done := range c.reducers {
-				shuffles, ok := c.mapperShuffle[k]
-				if !ok || len(shuffles) == 0 {
-					c.reducers[k] = true
-					continue
-				}
-				if done {
-					continue
-				}
-				allDone = false
-
-				// 1. 获取一个worker
-				workerAddr := c.applyWorker()
-				if strings.Compare(workerAddr, "") == 0 {
-					time.Sleep(1 * time.Second)
-					continue
-				}
-
-				// 2. 调用worker的ReduceReq
-				go func() {
-					success := CallReduceReq(workerAddr, k, shuffles)
-					if !success {
-						c.releaseWorker(workerAddr)
-						fmt.Println("call fail release addr:", workerAddr)
-					}
-				}()
-				time.Sleep(1 * time.Second)
-			}
-			if allDone {
-				break
-			}
-			fmt.Println("in reduce")
-			time.Sleep(1 * time.Second)
-		}
-
-		fmt.Println("task done")
-	}()
-
+	c := InitCoordinator(files, nReduce)
 	c.server()
+	return c
+}
+
+func InitCoordinator(files []string, nReduce int) *Coordinator {
+	c := Coordinator{}
+	c.files = files
+	c.nReduce = nReduce
+
 	return &c
 }
 
